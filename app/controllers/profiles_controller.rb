2b166fb7fbfd8a66deff5b55ea75b6b5ef41bb98
松下　佳憲 <yoshi@111222pc.local>
2024-08-14 17:40:11 +0900

    profile_edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,25 @@
 class ProfilesController < ApplicationController
   before_action :set_user, only: %i[edit update]
+  before_action :combine_birthdate_params, only: [:update]
 
   def edit; end
 
   def update
-    if @user.update(user_params)
-      redirect_to profile_path, success: 'プロフィールを更新しました。'
+    selected_areas = params[:user][:area_ids].reject(&:blank?)
+    selected_instruments = params[:user][:instrument_ids].reject(&:blank?)
+
+    if selected_areas.uniq.length != selected_areas.length
+      flash[:danger] = "選択したエリアに重複があります。"
+      redirect_to edit_profile_path
+    elsif selected_instruments.uniq.length != selected_instruments.length
+      flash[:dager] = "選択した楽器に重複があります。"
+      redirect_to edit_profile_path
     else
-      flash[:danger]
+      if @user.update(user_params)
+        redirect_to profile_path, success: 'プロフィールを更新しました。'
+      else
+        flash[:danger]
+      end
     end
   end
 
@@ -19,7 +31,18 @@ class ProfilesController < ApplicationController
     @user = User.find(current_user.id)
   end
 
+  def combine_birthdate_params
+    if params[:user][:birth_year].present? && params[:user][:birth_month].present? && params[:user][:birth_day].present?
+      birthdate = Date.new(
+        params[:user][:birth_year].to_i,
+        params[:user][:birth_month].to_i,
+        params[:user][:birth_day].to_i
+      )
+      params[:user][:birthdate] = birthdate
+    end
+  end
+
   def user_params
-    params.require(:user).premit(:name, :birthdate, :gender, :introduction, :profile_image, :profile_image_cache, :x_link, :instagram_link, :youtube_link, :custom_link)
+    params.require(:user).permit(:name, :birthdate, :gender, :purpose, :introduction, :profile_image, :profile_image_cache, :x_link, :instagram_link, :youtube_link, :custom_link, area_ids: [], instrument_ids: [])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,39 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, uniqueness: true
 
-  GENDER_OPTIONS = {
-    "男性" => "male",
-    "女性" => "female",
-    "その他" => "other"
-  }.freeze
+  has_many :user_areas
+  has_many :areas, through: :user_areas
+  has_many :user_instruments
+  has_many :instruments, through: :user_instruments
+
+  def age
+    return unless birthdate.present?
+    today = Date.today
+    age = today.year - birthdate.year
+    age -= 1 if today < birthdate + age.years
+    age
+  end
+
+  def gender_japanese
+    case gender
+    when 'male'
+      '男性'
+    when 'female'
+      '女性'
+    else
+      '未設定'
+    end
+  end
+
+  # 目的を日本語に変換するメソッド
+  def purpose_japanese
+    case purpose
+    when 'pro'
+      'プロ志望'
+    when 'hobby'
+      '趣味'
+    else
+      '未設定'
+    end
+  end
 end
