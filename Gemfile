e77248340a0f8159dece6b108662a1721268584b
松下　佳憲 <yoshi@111222pc.local>
2024-08-21 13:22:13 +0900

    メタタグ修正

--- Gemfile
+++ Gemfile
@@ -55,6 +55,8 @@ gem 'itunes-search-api'
 gem "httparty"
 
 gem 'mini_magick'
+
+gem 'meta-tags'
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,23 @@ class ApplicationController < ActionController::Base
   def not_authenticated
     redirect_to login_path
   end
+
+  def prepare_meta_tags(options = {})
+    defaults = {
+      site:        "MeTRO NOTE",
+      description: "完全実力主義のバンドメンバーマッチングアプリ",
+      og: {
+        site_name: "MeTRO NOTE",
+        description: "完全実力主義のバンドメンバーマッチングアプリ",
+        url: "https://metronote-a37794a02853.herokuapp.com"
+      },
+      twitter: {
+        card: "summary_large_image"
+      }
+    }
+
+    options.reverse_merge!(defaults)
+
+    set_meta_tags options
+  end
 end
